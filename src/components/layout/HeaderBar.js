8eb7f63ec0caee437ad63ee73cab4de00ad379ae
yangshanlong <yangshanlong@ssya.com>
2020-12-23 23:11:53 +0800

    修改注释

--- src/components/layout/HeaderBar.js
+++ src/components/layout/HeaderBar.js
@@ -12,13 +12,13 @@ const HeaderBar = (props) => {
 		<Header>
 			<div className="headBg">
 				<StDiolog></StDiolog>
-				{/* <div className="refresh">
+				<div className="refresh">
 					<img className="refresh-img" src={imgRefresh}/>
 					<span className="refresh-tips">数据源每日03:00:00更新，</span>
 					<span className="refresh-tips tips-other">最近一次数据同步于2020年06月22日14:51</span>
 					<span className="refresh-tips">，如数据已发生变化需要立即更新，请点击“立即同步”</span>    
 					<button className="refresh-btn">立即同步</button>         
-				</div>  */}
+				</div> 
 			</div> 
 		</Header>
   	);
